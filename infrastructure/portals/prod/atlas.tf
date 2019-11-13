409a59e29e1247feecb8e358cd06da91bbb6a0a5
kendavis2 <kenneth.davis2@turner.com>
2019-11-13 16:11:52 -0500

    updating setup docs

--- infrastructure/portals/prod/atlas.tf
+++ infrastructure/portals/prod/atlas.tf
@@ -28,7 +28,7 @@ variable "app_user_database" {}
 variable "ip_whitelist" {}
 
 module "env" {
-  source = "../../modules/atlas"
+  source = "github.com/turnerlabs/udeploy//infrastructure/modules/atlas?ref=v0.28.0-rc"
 
   mongodbatlas_public_key = var.mongodbatlas_public_key
 
