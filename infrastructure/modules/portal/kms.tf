e93340b54fb7ebde4e6dafa656da97e4f4a8bd50
kendavis2 <kenneth.davis2@turner.com>
2020-06-30 13:04:42 -0400

    enable iam users permissions for kms to support auditing

--- infrastructure/modules/portal/kms.tf
+++ infrastructure/modules/portal/kms.tf
@@ -1,65 +1,19 @@
 locals {
-  # KMS write actions
-  kms_write_actions = [
-    "kms:CancelKeyDeletion",
-    "kms:CreateAlias",
-    "kms:CreateGrant",
-    "kms:CreateKey",
-    "kms:DeleteAlias",
-    "kms:DeleteImportedKeyMaterial",
-    "kms:DisableKey",
-    "kms:DisableKeyRotation",
-    "kms:EnableKey",
-    "kms:EnableKeyRotation",
-    "kms:Encrypt",
-    "kms:GenerateDataKey",
-    "kms:GenerateDataKeyWithoutPlaintext",
-    "kms:GenerateRandom",
-    "kms:GetKeyPolicy",
-    "kms:GetKeyRotationStatus",
-    "kms:GetParametersForImport",
-    "kms:ImportKeyMaterial",
-    "kms:PutKeyPolicy",
-    "kms:ReEncryptFrom",
-    "kms:ReEncryptTo",
-    "kms:RetireGrant",
-    "kms:RevokeGrant",
-    "kms:ScheduleKeyDeletion",
-    "kms:TagResource",
-    "kms:UntagResource",
-    "kms:UpdateAlias",
-    "kms:UpdateKeyDescription",
-  ]
+    # list of saml users for policies
+    configUserIds = flatten([
+        data.aws_caller_identity.current.account_id,
+        formatlist(
+        "%s:%s",
+        data.aws_iam_role.saml_role_config.unique_id,
+        var.saml_users,
+        )
+    ])
 
-  # KMS read actions
-  kms_read_actions = [
-    "kms:Decrypt",
-    "kms:DescribeKey",
-    "kms:List*",
-  ]
-
-  # list of saml users for policies
-  saml_user_ids = flatten([
-    data.aws_caller_identity.current.user_id,
-    data.aws_caller_identity.current.account_id,
-    "${aws_iam_role.app_role.unique_id}:*",
-    formatlist(
-      "%s:%s",
-      data.aws_iam_role.saml_role_config.unique_id,
-      var.saml_users,
-    )
-  ])
-
-  # list of role users and saml users for policies
-  role_and_saml_ids = flatten([
-    "${aws_iam_role.ecsTaskExecutionRole.unique_id}:*",
-    local.saml_user_ids,
-  ])
-}
-
-# get the saml user info so we can get the unique_id
-data "aws_iam_role" "saml_role_config" {
-  name = var.saml_role
+    # list of role users and saml users for policies
+    configRoleIds = flatten([
+        "${aws_iam_role.ecsTaskExecutionRole.unique_id}:*",
+        "${aws_iam_role.app_role.unique_id}:*", 
+    ])
 }
 
 # The users (email addresses) from the saml role to give access
@@ -68,13 +22,19 @@ variable "saml_users" {
   type = list(string)
 }
 
-# kms key used to encrypt configuration
+# get the saml user info so we can get the unique_id
+data "aws_iam_role" "saml_role_config" {
+  name = var.saml_role
+}
+
 resource "aws_kms_key" "config" {
-  description             = "configuration key for ${var.app}-${var.environment}"
   deletion_window_in_days = 7
+  
   enable_key_rotation     = true
-  tags                    = var.tags
-  policy                  = data.aws_iam_policy_document.config.json
+  
+  tags = var.tags
+
+  policy = data.template_file.config_policy.rendered
 }
 
 resource "aws_kms_alias" "config" {
@@ -82,85 +42,155 @@ resource "aws_kms_alias" "config" {
   target_key_id = aws_kms_key.config.id
 }
 
-data "aws_iam_policy_document" "config" {
-  statement {
-    sid    = "DenyWriteToAllExceptSAMLUsers"
-    effect = "Deny"
-
-    principals {
-      type        = "AWS"
-      identifiers = ["*"]
-    }
-
-    actions   = local.kms_write_actions
-    resources = ["*"]
-
-    condition {
-      test     = "StringNotLike"
-      variable = "aws:userId"
-      values   = local.saml_user_ids
-    }
+data "template_file" "config_policy" {
+  template = <<EOF
+  {
+      "Version": "2012-10-17",
+      "Statement": [
+          {
+              "Sid": "DenyWriteToAllExceptSAMLUsers",
+              "Effect": "Deny",
+              "Principal": {
+                  "AWS": "*"
+              },
+              "Action": [
+                  "kms:UpdateKeyDescription",
+                  "kms:UpdateAlias",
+                  "kms:UntagResource",
+                  "kms:TagResource",
+                  "kms:ScheduleKeyDeletion",
+                  "kms:RevokeGrant",
+                  "kms:RetireGrant",
+                  "kms:ReEncryptTo",
+                  "kms:ReEncryptFrom",
+                  "kms:PutKeyPolicy",
+                  "kms:ImportKeyMaterial",
+                  "kms:GetParametersForImport",
+                  "kms:GenerateRandom",
+                  "kms:GenerateDataKeyWithoutPlaintext",
+                  "kms:GenerateDataKey",
+                  "kms:EnableKeyRotation",
+                  "kms:EnableKey",
+                  "kms:Encrypt",
+                  "kms:DisableKeyRotation",
+                  "kms:DisableKey",
+                  "kms:DeleteImportedKeyMaterial",
+                  "kms:DeleteAlias",
+                  "kms:CreateKey",
+                  "kms:CreateGrant",
+                  "kms:CreateAlias",
+                  "kms:CancelKeyDeletion"
+              ],
+              "Resource": "*",
+              "Condition": {
+                  "StringNotLike": {
+                      "aws:userId": $${writePrincipals}
+                  }
+              }
+          },
+          {
+              "Sid": "DenyReadToAllExceptRoleAndSAMLUsers",
+              "Effect": "Deny",
+              "Principal": {
+                  "AWS": "*"
+              },
+              "Action": [
+                  "kms:Decrypt"
+              ],
+              "Resource": "*",
+              "Condition": {
+                  "StringNotLike": {
+                      "aws:userId": $${readPrincipals}
+                  }
+              }
+          },
+          {
+              "Sid": "AllowWriteToSAMLUsers",
+              "Effect": "Allow",
+              "Principal": {
+                  "AWS": "*"
+              },
+              "Action": [
+                  "kms:UpdateKeyDescription",
+                  "kms:UpdateAlias",
+                  "kms:UntagResource",
+                  "kms:TagResource",
+                  "kms:ScheduleKeyDeletion",
+                  "kms:RevokeGrant",
+                  "kms:RetireGrant",
+                  "kms:ReEncryptTo",
+                  "kms:ReEncryptFrom",
+                  "kms:PutKeyPolicy",
+                  "kms:ImportKeyMaterial",
+                  "kms:GetParametersForImport",
+                  "kms:GenerateRandom",
+                  "kms:GenerateDataKeyWithoutPlaintext",
+                  "kms:GenerateDataKey",
+                  "kms:EnableKeyRotation",
+                  "kms:EnableKey",
+                  "kms:Encrypt",
+                  "kms:DisableKeyRotation",
+                  "kms:DisableKey",
+                  "kms:DeleteImportedKeyMaterial",
+                  "kms:DeleteAlias",
+                  "kms:CreateKey",
+                  "kms:CreateGrant",
+                  "kms:CreateAlias",
+                  "kms:CancelKeyDeletion"
+              ],
+              "Resource": "*",
+              "Condition": {
+                  "StringLike": {
+                      "aws:userId": $${writePrincipals}
+                  }
+              }
+          },
+          {
+              "Sid": "AllowReadRoleAndSAMLUsers",
+              "Effect": "Allow",
+              "Principal": {
+                  "AWS": "*"
+              },
+              "Action": [
+                  "kms:Decrypt"
+              ],
+              "Resource": "*",
+              "Condition": {
+                  "StringLike": {
+                      "aws:userId": $${readPrincipals}
+                  }
+              }
+          },
+          {
+            "Sid": "Enable IAM User Permissions",
+            "Effect": "Allow",
+            "Principal": {
+                "AWS": "arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"
+            },
+            "Action": [
+                "kms:DescribeKey",
+                "kms:GetKeyPolicy",
+                "kms:GetKeyRotationStatus",
+                "kms:List*"
+            ],
+            "Resource": "*"
+          }
+      ]
   }
+  EOF
 
-  statement {
-    sid    = "DenyReadToAllExceptRoleAndSAMLUsers"
-    effect = "Deny"
+  vars = {
+    writePrincipals = jsonencode(local.configUserIds)
 
-    principals {
-      type        = "AWS"
-      identifiers = ["*"]
-    }
-
-    actions   = local.kms_read_actions
-    resources = ["*"]
-
-    condition {
-      test     = "StringNotLike"
-      variable = "aws:userId"
-      values   = local.role_and_saml_ids
-    }
-  }
-
-  statement {
-    sid    = "AllowWriteToSAMLUsers"
-    effect = "Allow"
-
-    principals {
-      type        = "AWS"
-      identifiers = ["*"]
-    }
-
-    actions   = local.kms_write_actions
-    resources = ["*"]
-
-    condition {
-      test     = "StringLike"
-      variable = "aws:userId"
-      values   = local.saml_user_ids
-    }
-  }
-
-  statement {
-    sid    = "AllowReadRoleAndSAMLUsers"
-    effect = "Allow"
-
-    principals {
-      type        = "AWS"
-      identifiers = ["*"]
-    }
-
-    actions   = local.kms_read_actions
-    resources = ["*"]
-
-    condition {
-      test     = "StringLike"
-      variable = "aws:userId"
-      values   = local.role_and_saml_ids
-    }
+    readPrincipals = jsonencode(
+      concat(
+        local.configUserIds,
+        local.configRoleIds,
+      ),
+    )
   }
 }
 
 output "config_key_id" {
   value = aws_kms_key.config.key_id
 }
-
